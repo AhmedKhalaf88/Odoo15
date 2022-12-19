9aafea4c0558ad7ed0085fff2a5c6e1473e75e07
unknown <ahmed.khalaf.1088@gmail.com>
2022-12-19 17:00:18 +0100

    Create Odoo Project

---
+++ crm_inheritance/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "crm_inheritance",
+    'sequence': -100,
+    'summary':"",
+    'description': "",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['crm'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/crm_lead_view.xml',
+        'views/checklist.xml'
+    ],
+    'demo': [],
+}
---
+++ crm_inheritance/models/crm_lead.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    @api.depends('check_ids')
+    def checklist_progress(self):
+        total_len = self.env['check.list'].search_count([])
+        for rec in self:
+            if total_len != 0:
+                check_list_len = len(rec.check_ids)
+                rec.progress = (check_list_len * 100) / total_len
+            else:
+                rec.progress = 0
+
+    check_ids = fields.Many2many('check.list', string="List")
+
+    progress = fields.Float(compute=checklist_progress, string="Progress",  default=0.0)
+
+
+
+
+
+class CheckList(models.Model):
+    # _inherit = 'crm.lead'
+    _name = "check.list"
+    _description = "Checklist"
+
+    name = fields.Char(string='Name', required=True)
+
